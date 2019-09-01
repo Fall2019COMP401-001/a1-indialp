dcaabb1330b3493d99dcf2c51c665510dcccba70
indialp <indialp@152.23.132.235>
2019-09-01 17:03:40 -0400

    correct output

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -9,10 +9,9 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 		
 		int storeTotal = scan.nextInt(); // scans number of items at store
-		
 		String storeItemName[] = new String[storeTotal]; // creates array for all store items
 		double storeItemPrices[] = new double[storeTotal]; // creates array for all store item prices
-		
+
 		for (int i=0; i<storeTotal; i++) {
 			String itemNameScan = scan.next(); // scans for string item
 			storeItemName[i] = itemNameScan;
@@ -21,42 +20,112 @@ public class A1Adept {
 			storeItemPrices[i] = storePriceScan;
 		}
 		
+		
+
 		int customerCount = scan.nextInt(); // scans for total num of customers
+		
 		String customerNames[] = new String[customerCount]; // array for customer name
 		double customerPaid[] = new double[customerCount]; // array for how much each customer paid
 		
-		for (int x=0; x<customerCount; x++) {
-			
+		for (int w=0; w<customerCount; w++) {
 			String firstName = scan.next(); // scans for first name
 			String lastName = scan.next(); // scans for last name
+			customerNames[w] = firstName + " " + lastName; // stores first and last name into array
 			
-			customerNames[x] = firstName + " " + lastName; // stores first and last name into array
+			int itemNumCustomer = scan.nextInt(); // how many different items customer bought
 			
-			int itemNumCustomer = scan.nextInt(); // scans how many diff items bought
-			double customerTotal = 0; // initialize how much customer paid in total
-			double totalCost = 0; // how much all the customers paid
-			
-				for (int y=0; y<itemNumCustomer; y++) {
+			for (int y=0; y<itemNumCustomer; y++) {
+				int numBought = scan.nextInt(); // scans how many of item was bought
+				String itemName = scan.next(); // scans name of item bought
 				
-					int numBought = scan.nextInt(); // scans how many of item was bought
-					String itemName = scan.next(); // scans name of item bought
-					
-					//SEARCH ARRAY HERE
-					for (int z=0; z<storeItemName.length; z++) {
-						if (storeItemName[z] == itemName) { // finds item name in array of store items
-							customerTotal = customerTotal + (storeItemPrices[z]*numBought); // how much customer has spent
-						}
-					}
-					customerPaid[y] = customerTotal; // 
-					//ADD HOW MUCH CUSTOMER SPEND TO ARRAY CUSTOMER PAID
-					
+				for (int z=0; z<storeTotal; z++) {
+					if (itemName.equals(storeItemName[z]) ) 
+	                {customerPaid[w] += (numBought * storeItemPrices[z]);}
 				}
-			
-			
+
+			}
+
+			//System.out.println(customerNames[w] + " " + String.format("%.2f", customerPaid[w]));
 		}
+		double max = findValueMax(customerPaid);
+		//System.out.println(String.format("%.2f",max));
+		double min = findValueMin(customerPaid);
+		//System.out.println(String.format("%.2f",min));
+		double sum = calculateValueSum(customerPaid);
+		//System.out.println(String.format("%.2f", ((double) sum) / ((double) customerCount)));
+		
+		for (int g=0; g<customerCount; g++) {
+			if (max == customerPaid[g] ) {
+				System.out.println("Biggest: " + customerNames[g] + " " + "(" + String.format("%.2f",max) + ")");
+			}
+		}
+		for (int g=0; g<customerCount; g++) {
+			if (min == customerPaid[g] ) {
+				System.out.println("Smallest: " + customerNames[g] + " " + "(" + String.format("%.2f",min) + ")");
+			}
+		}
+		System.out.println("Average: " + String.format("%.2f", ((double) sum) / ((double) customerCount)));
 		
 		scan.close();
 		
-	}	
+		
 }
 
+	static double calculateValueSum(double[] vals) {
+		
+		double sum = 0;
+		
+		for (int i=0; i<vals.length; i++) {
+			sum += vals[i];
+		}
+		
+		return sum;
+	}
+	
+	
+	static double findValueMin(double[] vals) {
+		
+		// Initialize current minimum to first value in array.
+		double cur_min = vals[0];
+		
+		// Starting with second value (if any), compare each value
+		// in array with current minimum and replace if smaller.
+		
+		for (int i=1; i < vals.length; i++) {
+			if (vals[i] < cur_min) {
+				cur_min = vals[i];
+			}
+		}
+		
+		return cur_min;
+	}
+
+	/* findValueMax
+	 * Finds and returns the maximum value in an array of integers
+	 * 
+	 * Input: array of integers vals
+	 * 
+	 * Output: maximum value found within the array
+	 * 
+	 * Preconditions:
+	 * Input array must not be null and must contain at least one value.
+	 */
+	
+	static double findValueMax(double[] vals) {
+		
+		// Initialize current minimum to first value in array.
+		double cur_max = vals[0];
+		
+		// Starting with second value (if any), compare each value
+		// in array with current minimum and replace if smaller.
+		
+		for (int i=1; i < vals.length; i++) {
+			if (vals[i] > cur_max) {
+				cur_max = vals[i];
+			}
+		}
+		
+		return cur_max;
+	}
+
+}
